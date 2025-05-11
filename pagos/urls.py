a2dfab7d98e63091a2e1448a223111c17a783dfd
carlos22709 <carlossanchez22709@gmail.com>
2025-05-11 15:05:16 -0500

    :bug: arreglado pago para admin

--- pagos/urls.py
+++ pagos/urls.py
@@ -1,13 +1,10 @@
-# pagos/urls.py
 from django.urls import path
-
-from pagos.views import PagoListUsuarioView, vista_crear_pago
 from .views import (
     PagoListView,
     PagoListUsuarioView,
+    PagoDetailView,
     PagoUpdateView,
     PagoDeleteView,
-    PagoDetailView,
     registrar_mensualidad,
     vista_crear_pago,
 )
@@ -15,8 +12,8 @@ from .views import (
 app_name = 'pagos'
 
 urlpatterns = [
-    path('', PagoListView.as_view(), name='list'),  # Para admin
-    path('mis-pagos/<int:usuario_id>/', PagoListUsuarioView.as_view(), name='mis_pagos'),
+    path('', PagoListView.as_view(), name='list'),  # Vista para admin
+    path('mis-pagos/<int:usuario_id>/', PagoListUsuarioView.as_view(), name='mis_pagos'),  # Vista para miembros
     path('crear/', vista_crear_pago, name='crear'),
     path('detalle/<int:pk>/', PagoDetailView.as_view(), name='detalle'),
     path('editar/<int:pk>/', PagoUpdateView.as_view(), name='editar'),
